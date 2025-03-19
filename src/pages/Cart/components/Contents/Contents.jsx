27ca910ea083ecf6d3ec27e434b7c3fe00d698ad
unknown <you@example.com>
2025-03-19 21:28:42 +0700

    Làm phần CouPon

--- src/pages/Cart/components/Contents/Contents.jsx
+++ src/pages/Cart/components/Contents/Contents.jsx
@@ -3,15 +3,40 @@ import styles from '../../styles.module.scss'
 import CartTable from './CartTable'
 import CartSummary from './CartSummary'
 import SafeCheckout from './SafeCheckOut'
+import Button from '@components/Button/Button'
+import { FaRegTrashCan } from 'react-icons/fa6'
 
 function Contents() {
-    const { containerContent, contentLeft, contentRight} = styles
+    const {
+        containerContent,
+        contentLeft,
+        contentRight,
+        coupon,
+        couponSearch,
+        couponClear
+    } = styles
     return (
         <>
             <MainLayout>
                 <div className={containerContent}>
                     <div className={contentLeft}>
                         <CartTable />
+                        <div className={coupon}>
+                            <div className={couponSearch}>
+                                <input type="text" placeholder="Coupon code" />
+                                <input type="submit" value="OK" />
+                            </div>
+                            <div className={couponClear}>
+                                <Button
+                                    content={
+                                        <div>
+                                            <FaRegTrashCan />{' '}
+                                            {'Clear shopping cart'}
+                                        </div>
+                                    }
+                                />
+                            </div>
+                        </div>
                     </div>
                     <div className={contentRight}>
                         <CartSummary />
